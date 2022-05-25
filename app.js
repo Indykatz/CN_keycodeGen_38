e8ac695ddc7cc47dc6954b37900a6e392cac41ae
Indykatz <cats83@me.com>
2022-05-25 11:30:04 +0100

    edit 1

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-page2.style.display = "none";
+// JS for keycode gen
+
+const textArea = document.getElementById("textArea");
+const key = document.getElementById("key");
+const which = document.getElementById("which");
+const code = document.getElementById("code");
 
 window.addEventListener("keydown", (e) => {
-  const page1 = document.getElementById("page1");
-  const page2 = document.getElementById("page2");
-
-  page1.style.display = "none";
-  page2.style.display = "flex";
-
-  const key = document.getElementById("key");
-  //   const location = document.getElementById("location");
-  const which = document.getElementById("which");
-  const code = document.getElementById("code");
-
   key.textContent = e.key;
-  //   location.textContent = e.location;
   which.textContent = e.which;
   code.textContent = e.code;
+
+  textArea.textContent = `${e.key} was pressed`;
 });
