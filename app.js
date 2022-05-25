2f552df6f747bec8c2e6ccf6fd4b6fea5e1916ab
Indykatz <cats83@me.com>
2022-05-26 00:56:09 +0100

    styled v1

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const textArea = document.getElementById("textArea");
 const key = document.getElementById("key");
 const which = document.getElementById("which");
 const code = document.getElementById("code");
+const displayNum = document.getElementById("displayNum");
+const returnStr = document.getElementById("returnStr");
 
 window.addEventListener("keydown", (e) => {
   key.textContent = e.key;
   which.textContent = e.which;
   code.textContent = e.code;
 
-  textArea.textContent = `"${e.key}" was pressed`;
+  displayNum.textContent = e.which;
+  returnStr.textContent = `"${e.key}" was pressed`;
 });
